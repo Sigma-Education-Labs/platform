42f17a04063ccf7332c747b20cbf23c56b2f7239
stoyan <smadjaroff99@gmail.com>
2022-08-17 17:38:33 +0300

    First changes.

--- map/views.py
+++ map/views.py
@@ -1,3 +1,4 @@
+from doctest import script_from_examples
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,4 +6,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Map index.")
+    return HttpResponse("Welcome to the Map index page. Currently in development.")
+
+def map_view(request):
+      
+    # render function takes argument  - request
+    # and return HTML as response
+    return render(request, "./templates/map/map.html")
