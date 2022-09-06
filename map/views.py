cabdeb18a52484c9e8af65a0f03b8962282eb20a
Vasilen Alexandrov <vasilen.alexandrov@gmail.com>
2022-09-06 14:29:36 +0300

    Reading fire coordinates from a RPi

--- map/views.py
+++ map/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.template import loader
 import ibm_boto3
 from ibm_botocore.client import Config, ClientError
+import subprocess
+import os
+from os.path import exists
 
 def get_item(bucket_name, item_name):
     print("Retrieving item from bucket: {0}, key: {1}".format(bucket_name, item_name))
@@ -14,22 +17,26 @@ def get_item(bucket_name, item_name):
     except Exception as e:
         print("Unable to retrieve file contents: {0}".format(e))
 
+def test_get_fire_coords_from_pi():
+    os.system('rm /Users/mount40/Projects/sigma/sigma/map/resources/fires.txt')
+    os.system('sshpass -p "toor" scp root@10.0.0.1:/work/fires.txt /Users/mount40/Projects/sigma/sigma/map/resources')
 
 def index(request):
+    test_get_fire_coords_from_pi()
     fire_coords = []
     s3_bucket_name = ""
-    coords_file_name = "map/coords-01-09-2022.txt"
+    coords_file_name = "map/resources/fires.txt"
 
     # Uncomment when we have access to an IBM COS S3
     # coords_file = get_item(s3_bucket_name, coords_file_name)
-    
-    with open(coords_file_name) as f:
-        contents = f.readlines()
-        for i in contents:
-            coords_pair = i.split()
-            # First part of the pair is lat and if it's bellow 30 - it might be a desert
-            # in which case we don't want it (for now)
-            if float(coords_pair[0]) > 30.0:
+    if exists(coords_file_name):
+        with open(coords_file_name) as f:
+            contents = f.readlines()
+            for i in contents:
+                coords_pair = i.split()
+                # First part of the pair is lat and if it's bellow 30 - it might be a desert
+                # in which case we don't want it (for now)
+                # if float(coords_pair[0]) > 30.0:
                 fire_coords.append(coords_pair)
 
     template = loader.get_template('map/map.html')
